96fc4b4eb074d4443130f1e6a4cecc871fb65871
Manjari9807 <105922167+Manjari9807@users.noreply.github.com>
2022-07-14 13:07:25 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,21 @@
+package MaheshSan;
+
+public class Array {
+
+	public static void main(String[] args) {
+		int a[] = { 1, 2, 3, 4, 5, 6 };
+		int k = 5;
+
+		for (int i = 0; i < a.length - 1; i++) {
+			for (int j = i + 1; j < a.length; j++) {
+				if ((a[i] + a[j]) % k == 0) {
+
+					System.out.println(a[i] + "," + a[j]);
+				}
+
+			}
+		}
+
+	}
+
+}
